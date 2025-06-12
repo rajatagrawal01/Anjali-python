ae46467b7d0edc7afe0d973fae4a025dafbecb93
rajatagrawal01 <rajatagrawal01@gmail.com>
2025-06-12 16:29:22 +0530

    first commit

---
+++ first_python.py
@@ -0,0 +1,10 @@
+a="Hello, World!"
+b=23.3
+c=True
+
+print("Value of a is",a)
+print("sdkfbsd,nmfbsdnf,sbdfn",b)
+
+print("Type of a is",type(a))
+print("Type of b is",type(b))
+print("Type of c is",type(c))
