15ec110c6ec19b7fb7e091820aab5e82737daf6b
rajatagrawal01 <rajatagrawal01@gmail.com>
2025-06-14 16:59:51 +0530

    Operators

---
+++ operators.py
@@ -0,0 +1,37 @@
+# Arithmetic Operators Example
+
+# a=7
+# b=2
+# print("Addition of", a," and", b, "is",a+b)
+# print("Subtraction of", a," and", b, "is",a-b)
+# print("Multiplication of", a," and", b, "is",a*b)
+# print("Division of", a," and", b, "is",a/b)
+# print("Floor Division of", a," and", b, "is",a//b)
+# print("Modulus of", a," and", b, "is",a%b)
+# print("Result of", a," to the power", b, "is",a**b)
+
+# Assignment Operators Example
+# +,+=,-=,*,/=,//=,%=,**=
+# a=5
+# a**=2
+# print("Value of a is", a)
+
+# Comparison Operators Example
+# ==,!=,>,<,>=,<=
+# a=5
+# b=5
+# print("Is a equal to b?", a == b) #False
+# print("Is a not equal to b?", a != b) #True
+# print("Is a greater than b?", a > b) #True
+# print("Is a less than b?", a < b) #False
+# print("Is a greater than or equal to b?", a >= b) #True
+# print("Is a less than or equal to b?", a <= b) #False
+
+#Logical Operators
+
+# a=5
+# print("Using 'or' operator",a%3==0 or a%5==0)
+
+# print("Using 'and' operator",a%3==0 and a%5==0)
+
+#Increment / Decrement Operator (a++,a--)
