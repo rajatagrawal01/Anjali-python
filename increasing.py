6181178aa08fe7a4896ff17a3655bbcf15aae501
rajatagrawal01 <rajatagrawal01@gmail.com>
2025-07-15 16:01:31 +0530

    practice

---
+++ increasing.py
@@ -0,0 +1,29 @@
+# n=23432
+# result=1
+# prev=10
+
+# while n>0:
+#     current = n%10 
+#     if current>prev:
+#         result=0
+#         break
+#     prev = current
+#     n=n//10
+
+# print(count)
+
+
+count=0
+while n>0:
+    current = n%10 # 0
+    if result==1:
+        count+=1
+    prev = current #4
+    n=n//10 #0
+
+print(count)
+prime_sum=[52,346,245,23234,234234,23424,23424,224]
+for i in range(1,1001):
+
+
+print(prime_sum)
---
+++ list.py
@@ -0,0 +1,4 @@
+results=[1,2,3,4]
+print("Old",results)
+results.append(5)
+print("new",results)
