384e4d309665daf5f07507426e779db2ef0e903f
rajatagrawal01 <rajatagrawal01@gmail.com>
2025-07-19 16:36:58 +0530

    list

--- list.py
+++ list.py
@@ -1,4 +1,46 @@
-results=[1,2,3,4]
-print("Old",results)
-results.append(5)
-print("new",results)
+a=13
+b=[5,6,7,8,"Hello",True] #Array(All languages) - list
+c=[1,2,3,4,5,6,7,8,9,10]
+d=[]
+# print(b)
+# print(b[4])
+# print(b[-3])
+
+# for item in b:
+#     print(item)
+
+# for numb in c:
+#     if(numb%2==0):
+#         print(numb)
+
+# item=0
+# while item<len(b):
+#     print(b[item])
+#     item+=1
+
+c=[1,2,3,4,5,2,7,664,9,1]
+print("Length of list",len(c))
+print("Min value of list",min(c))
+print("Max value of list",max(c))
+print("Sum of all elements",sum(c))
+c.sort()
+print("Ascending",c)
+c.sort(reverse=True)
+print("Descending",c)
+
+
+# c=[0,10,2,3,4,5,2,7,664,9,1]
+# minimum=c[0]
+# for numb in c:
+#     if(numb>=minimum):
+#         continue
+#     else:
+#         minimum=numb-1
+# print("Minimum value using loop",minimum)
+# print("Min value using min method",min(c))
+
+
+
+
+
+
--- patterns.py
+++ patterns.py
@@ -1,7 +1,7 @@
-# *
-# **
-# ***
-# ****
+#     *
+#    **
+#   ***
+#  ****
 # *****
 
 # row - column
@@ -15,11 +15,18 @@
 #     for col in range(1,row):
 #         print("*")
 
-rows =5 
-for i in range(1,rows+1):
-    for j in range(1,i+1):
-        print("*",end='')
-    print()
+# rows =5 
+# for i in range(1,rows+1):
+#     for j in range(1,i+1):
+#         print("*",end='')
+#     print()
+
+
+# i-1 #(1,2) 1
+# i-2 #(1,3) 1,2
+# i-3 #(1,4) 1,2,3
+# i-4 #(1,4) 1,2,3,4
+# i-5 #(1,4) 1,2,3,4,5
 
 
 # 1-1
@@ -35,3 +42,12 @@ for i in range(1,rows+1):
 # ***
 # **
 # *
+
+numOfRows =6 
+for row in range(1,numOfRows):
+    for col in range(1,numOfRows-row):
+        print(" " , end=" ")
+    for col in range(1,row): #(1,4)
+        print("*",end=" ")
+    print()
+
