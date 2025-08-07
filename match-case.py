04b7ae661f1b8d5c2690a07073da8a7d08f7d051
rajatagrawal01 <rajatagrawal01@gmail.com>
2025-08-07 16:10:24 +0530

    match-case

---
+++ match-case.py
@@ -0,0 +1,43 @@
+# day = int(input("Enter day of week : "))
+
+# match day:
+#     case 1:
+#         print("Monday")
+#     case 2:
+#         print("Tuesday")
+#     case 3:
+#         print("Wednesday")
+#     case 4:
+#         print("Thursday")
+#     case 5:
+#         print("Friday")
+#     case 6:
+#         print("Saturday")
+#     case 7:
+#         print("Sunday")
+#     case _:
+#         print("Invalid day")
+
+
+
+# command= input("Please bhaiya command do : ")
+
+# match command:
+#     case "start" | "chalu karo":
+#         print("Okay bhaiya starting")
+#     case "end"  | "Band karo":
+#         print("Okay bhaiya ending")
+#     case "pause"  | "ruk jao":
+#         print("Okay bhaiya pausing")
+#     case _:
+#         print("Bhaiya galat command")
+
+
+
+# greet= input("Please do greeting : ")
+
+# match greet:
+#     case "Hi" | "Hello" | "Hola" | "Namaste" | "Bonjour":
+#         print("Welcome")
+#     case "Bye" | "Tata" | "See ya" | "asta lavista" | "sayonara" | "Jao":
+#         print("Bye")
