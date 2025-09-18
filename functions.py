f32136d68a72f0fa3c25ec650a04ac011f9b51a3
rajatagrawal01 <rajatagrawal01@gmail.com>
2025-09-18 16:15:50 +0530

    function basic

---
+++ functions.py
@@ -0,0 +1,24 @@
+# def function_name(parameters or params):
+#     function body
+#     return result
+
+# function_name = Name of the function
+# parameters = inputs required in a function
+# Body = logic code of a function
+# Return = result/output
+
+# def add(a,b):
+#     print(a+b)
+
+# def addition(a,b):
+#     return a+b
+
+def evenOddNo(a):
+    return a%2
+
+c=evenOddNo(2)
+
+if(c==0):
+    print("Even")
+else:
+    print("Odd")
