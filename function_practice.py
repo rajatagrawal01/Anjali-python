fb5965e0718c7525bdd6073d99d5cf19c6a3b0dd
rajatagrawal01 <rajatagrawal01@gmail.com>
2025-09-25 16:18:03 +0530

    function

---
+++ function_practice.py
@@ -0,0 +1,23 @@
+def even_odd(a):
+    if(a%2==0):
+        return f"Number is even"
+    else:
+        print("Number is odd")
+
+# even_odd(23)
+even_odd(22)
+
+
+# def list_ops(c):
+#     print("Length of list",len(c))
+#     print("Min value of list",min(c))
+#     print("Max value of list",max(c))
+#     print("Sum of all elements",sum(c))
+#     c.sort()
+#     print("Ascending",c)
+#     c.sort(reverse=True)
+#     print("Descending",c)   
+
+# a=[1,2,3,4,5,2,7,664,9,1]
+
+# list_ops(a)
--- questions.py
+++ questions.py
@@ -3,5 +3,10 @@
 # if x ** 3 > 100 and y < 100:
 #     print(x, y)
 
-print(2%6)
+# print(2%6)
 
+a=[1,2,3,4]
+b=a
+print("Before",b)
+a=[10,20,30,40]
+print("After",a ,"And",b)
