d8b5317b6c2c70504ac1feb1f32f4b8c99832e59
rajatagrawal01 <rajatagrawal01@gmail.com>
2025-11-04 16:07:59 +0530

    types to import module

--- check.py
+++
@@ -1,7 +0,0 @@
-import even
-
-# even.evenOdd(7)
-# even.calculator(24,12)
-
-even.percntage(23,99,45,23,57,67)
-
---
+++ logics.py
@@ -0,0 +1,16 @@
+# Different ways to import modules
+
+#1 Default common method
+# import modules
+
+#2 importing module by some other name
+# import modules as m
+
+#3 Importing single function from a module
+from modules import percntage
+
+# m.evenOdd(7)
+# m.calculator(24,12)
+
+percntage(23,99,45,23,57,67)
+
